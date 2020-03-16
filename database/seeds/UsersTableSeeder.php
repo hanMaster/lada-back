4ec12a2f0d91788c9d6560351a640bc86c2d0012
hanMaster <w54661c@gmail.com>
2020-03-16 18:36:37 +1000

    Airlock

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      User::insert([
+        'name' => 'han',
+        'email' => '1@2.com',
+        'password' => bcrypt('1qaz2wsx'),
+        'created_at' => Carbon::now(),
+        'updated_at' => Carbon::now(),
+    ]);
+    User::insert([
+        'name' => 'Администратор',
+        'email' => '2@2.com',
+        'password' => bcrypt('1qaz2wsx'),
+        'created_at' => Carbon::now(),
+        'updated_at' => Carbon::now(),
+    ]);
+    }
+}
