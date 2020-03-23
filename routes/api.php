a6b6e32e866d1ae87863016db85a81109bf50fff
hanMaster <w54661c@gmail.com>
2020-03-23 12:08:20 +1000

    Airlock renamed to Sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-define('AUTH_AIRLOCK', "auth:airlock");
+define('AUTH_SANCTUM', "auth:sanctum");
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,19 +15,18 @@ define('AUTH_AIRLOCK', "auth:airlock");
 |
 */
 
-Route::middleware(AUTH_AIRLOCK)->get('/profile','ProfileController@index');
+Route::middleware(AUTH_SANCTUM)->get('/profile','ProfileController@index');
 
 Route::get('filial/{filial}', 'FilialDoctorsController@filial')->name('filial');
 
 Route::get('filials', 'FilialDoctorsController@index')->name('filials');
 
-Route::get('events/{filial}', 'EventController@index')->middleware(AUTH_AIRLOCK);
-Route::get('doctors/{filial}', 'FilialDoctorsController@doctors')->middleware(AUTH_AIRLOCK);
+Route::get('events/{filial}', 'EventController@index')->middleware(AUTH_SANCTUM);
+Route::get('doctors/{filial}', 'FilialDoctorsController@doctors')->middleware(AUTH_SANCTUM);
 
 Route::post('event', 'EventController@store');
 
-Route::post('schedule', 'FilialDoctorsController@storeSchedule')->middleware(AUTH_AIRLOCK);
+Route::post('schedule', 'FilialDoctorsController@storeSchedule')->middleware(AUTH_SANCTUM);
 
-Route::post('spec', 'SpecController@store');
-//    ->middleware(AUTH_AIRLOCK);
-Route::put('spec/{spec}', 'SpecController@update')->middleware(AUTH_AIRLOCK);
+Route::post('spec', 'SpecController@store')->middleware(AUTH_SANCTUM);
+Route::put('spec/{spec}', 'SpecController@update')->middleware(AUTH_SANCTUM);
